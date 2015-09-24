224ee50e666f001d00a5cbdd595ce678a9b0cbe3
Kevin Wang <wangkevin448@gmail.com>
2015-09-24 01:15:04 -0700

    linked output for Layer

--- network.py
+++ network.py
@@ -6,6 +6,7 @@ def sigmoid(x, deriv = False):
     else:
         return 1/(1+np.exp(-x))
 
+
 class Neuron:
     def __init__(self, aWeights):
         '''weights should be a (L+1) vector'''
@@ -13,7 +14,7 @@ class Neuron:
 
     def out(self, input):
         '''input should be a (L+1) vector, should have a -1 as last value for threshold weight'''
-        return sigmoid(np.dot(input, self._weights)[0][0])
+        return sigmoid(np.dot(input, self._weights))
 
 
 class Layer:
@@ -21,13 +22,30 @@ class Layer:
         self._neurons = []
         self._neurLength = numNeurons
         self._nextLayer = None
+        self._prevLayer = None
 
     def connect(self, layer):
         '''A -> B : B.connect(A)'''
+        self._neurons = []
         weightLength = layer._neurLength + 1
         for i in range(0, self._neurLength):
-            self._neurons.append(Neuron(2*np.random.random((weightLength)) - 1))
+            self._neurons.append(Neuron(2*np.random.random(weightLength) - 1))
         layer._nextLayer = self
+        self._prevLayer = layer
+
+    def out(self, input):
+        '''input should be a (L) vector'''
+        inp = input.append(-1)
+        arr = np.array([])
+        for(neuron in self._neurons):
+            arr.append(neuron.out(inp))
+        return arr
+
+
+class Input(Layer):
+    def __init__(self, length):
+        self._inputs = np.array([])
+#need to finish input
 
 
 class Network:
@@ -47,5 +65,5 @@ class Network:
 To do:
 - input -> output
 - training
-- genetic algorithm 
+- genetic algorithm
 '''
