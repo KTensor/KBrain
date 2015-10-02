887a9d1b818942e471ff6543520bbe72ad97a7c8
Kevin Wang <wangkevin448@gmail.com>
2015-10-01 23:55:39 -0700

    complete gru

--- grunet.py
+++ grunet.py
@@ -5,5 +5,9 @@ import random as random
 #github:xorkevin
 
 class Unit:
-    pass
-    
+    def __init__(self, aSize):
+        self._size = aSize
+
+    def setWeights(self, zX, zH, rX, rH):
+        '''parameters: update input, update history, reset input, reset history; each should be an input length vector, history length vector'''
+        pass
